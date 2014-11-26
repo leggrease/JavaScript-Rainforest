0a37797fc7a3cd5bd5ac84eeb81f23c5a7406c6c
Manny Berkal-Sarbit <mannyberkalsarbit@gmail.com>
2014-11-26 16:40:40 -0500

    mission accomplished

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ActiveRecord::Base
+	validates :comment, :presence => true
+	scope :newest_first, -> {order("created_at DESC")}
+	
 	belongs_to :user
 	belongs_to :product
-
-	scope :newest_first, -> {order("created_at DESC")}
 end
